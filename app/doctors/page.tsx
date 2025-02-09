56c48cc1014865842a89fe2be6ea6059c117a837
Leon Carbonell <leoncarbonell@gmail.com>
2025-02-09 17:06:11 +1030

    added drizzle config for dev and prod

--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -5,7 +5,7 @@ import AddDoctorForm from './_components/add-doctor-form'
 
 export default async function DoctorsPage() {
   const doctorsTable = await db.select().from(doctors)
-  console.table(doctorsTable)
+
   return (
     <div className="min-h-[calc(100vh-3rem)]">
       <h1>Doctors</h1>
---
+++ drizzle-prod.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'drizzle-kit'
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './drizzle/schemas',
+  dbCredentials: {
+    url: process.env.DATABASE_URL_PROD!,
+  },
+})
