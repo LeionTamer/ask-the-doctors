ebafccb118f14cec5229cec10b8044461c11bb75
Leon Carbonell <leoncarbonell@gmail.com>
2025-02-14 07:04:19 +1030

    add get all urls logic

--- app/scholars/_actions/search-scholar-action.tsx
+++ app/scholars/_actions/search-scholar-action.tsx
@@ -1,28 +1,50 @@
 'use server'
 import * as cheerio from 'cheerio'
+import puppeteer from 'puppeteer'
 
 export async function searchScholarAction() {
-  const page = await fetch(
-    'https://scholar.google.com.au/citations?user=1R2uLp4AAAAJ'
-  )
+  const url = 'https://scholar.google.com.au/citations?user=1R2uLp4AAAAJ'
 
-  const $ = cheerio.load(await page.text())
-  const data = $.extract({
-    papers: [
-      {
-        selector: '.gsc_a_t',
-        title: {
-          selector: '.gsc_a_at',
-        },
-        link: {
-          selector: '.gsc_a_at',
-          attr: 'href',
-        },
-      },
-    ],
-  })
+  const urlSearchParams = new URLSearchParams(url.split('?')[1]) // split at '?' to just get query params
+  const userId = urlSearchParams.get('user')
 
-  const slicedData = data.papers.slice(2)
+  if (!userId) {
+    throw new Error('User ID not found')
+  }
 
-  console.table(slicedData[0])
+  const browser = await puppeteer.launch()
+  const scholarPage = await browser.newPage()
+  await scholarPage.goto(url)
+
+  const urls: string[] = []
+
+  try {
+    let hasMore =
+      (await scholarPage.$eval('#gsc_bpf_more', (el) =>
+        el.getAttribute('disabled')
+      )) === null
+
+    while (hasMore) {
+      await scholarPage.click('#gsc_bpf_more')
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      hasMore =
+        (await scholarPage.$eval('#gsc_bpf_more', (el) =>
+          el.getAttribute('disabled')
+        )) === null
+    }
+
+    const content = await scholarPage.content()
+    const $ = cheerio.load(content)
+
+    $('.gsc_a_tr').each((_, element) => {
+      const urlParams = $(element).find('.gsc_a_at').attr('href')
+      const url = `https://scholar.google.com.au${urlParams}`
+      urls.push(url)
+    })
+  } finally {
+    browser.close()
+  }
+
+  console.log('results')
+  console.table(urls)
 }
